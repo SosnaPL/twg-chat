75c066903754eb42f6c9157db94df19363474e86
SosnaPL <thematsupl@gmail.com>
2022-01-26 20:20:40 +0100

    added room related queries/mutations

--- queries/queries.tsx
+++ queries/queries.tsx
@@ -21,3 +21,25 @@ export const room_messages = gql`
     }
   }
 `;
+
+export const room_details = gql`
+  query last_message($id: String!) {
+      room(id: $id) {
+        messages {
+          body
+          id
+          user {
+            id
+        }
+      }
+    }
+  }
+`;
+
+export const send_message = gql`
+  mutation send_message($body: String!, $roomid: String!) {
+    sendMessage(body: $body, roomId: $roomid) {
+      id
+    }
+  }
+`
